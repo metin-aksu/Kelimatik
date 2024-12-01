8821383ac9af365b9ac27174f8aa28a229e4812c
metin-aksu <drmetin@msn.com>
2024-12-02 02:13:03 +0300

    search action done

--- src/db/database.ts
+++ src/db/database.ts
@@ -1,12 +1,18 @@
-// src/db/database.ts
 import SQLite from 'react-native-sqlite-storage';
 
 SQLite.enablePromise(true);
 
 export interface Word {
   id: number;
-  word: string;
+  kelime: string;
   boyut: number;
+}
+
+export enum SearchType {
+  EXACT = '1',
+  START = '2',
+  END = '3',
+  CONTAIN = '4',
 }
 
 export class DatabaseConnection {
@@ -22,36 +28,64 @@ export class DatabaseConnection {
 
       this.database = db;
       console.log('Database connected successfully!');
-
-      // Test query
-      const [results] = await this.database.executeSql(
-        'SELECT * FROM kelimeler LIMIT 1',
-      );
-      console.log(
-        'Database test query successful, found',
-        results.rows.length,
-        'rows',
-      );
     } catch (error) {
       console.error('Database connection error:', error);
       throw error;
     }
   }
 
-  async searchWords(letters: string): Promise<Word[]> {
+  async searchWords(letters: string, searchType: SearchType): Promise<Word[]> {
     if (!this.database) {
       await this.initDatabase();
     }
 
     try {
-      const [results] = await this.database!.executeSql(
-        'SELECT * FROM kelimeler WHERE kelime LIKE ? ORDER BY boyut DESC, kelime ASC',
-        [`%${letters}%`],
-      );
+      let query = 'SELECT * FROM kelimeler WHERE ';
+      let searchPattern: string;
+
+      switch (searchType) {
+        case SearchType.EXACT:
+          query = createSqlForExact(letters);
+          searchPattern = `${letters}%`;
+          break;
+        case SearchType.START:
+          query += 'kelime LIKE ?';
+          searchPattern = `${letters}%`;
+          break;
+        case SearchType.END:
+          query += 'kelime LIKE ?';
+          searchPattern = `%${letters}`;
+          break;
+        case SearchType.CONTAIN:
+          query += 'kelime LIKE ?';
+          searchPattern = `%${letters}%`;
+          break;
+        default:
+          throw new Error('Geçersiz arama tipi');
+      }
+
+      query += ' ORDER BY boyut DESC, kelime ASC';
+
+      let res;
+      if (searchType === SearchType.EXACT) {
+        res = await this.database!.executeSql(query);
+      } else {
+        res = await this.database!.executeSql(query, [searchPattern]);
+      }
+      const [results] = res;
 
       const words: Word[] = [];
       for (let i = 0; i < results.rows.length; i++) {
-        words.push(results.rows.item(i));
+        let word = results.rows.item(i).kelime;
+        if (word.length > 2) {
+          if (searchType === SearchType.EXACT) {
+            if (canFormWord(word, letters)) {
+              words.push(results.rows.item(i));
+            }
+          } else {
+            words.push(results.rows.item(i));
+          }
+        }
       }
       return words;
     } catch (error) {
@@ -59,6 +93,47 @@ export class DatabaseConnection {
       return [];
     }
   }
+}
+
+function createSqlForExact(inputLetters) {
+  const alphabet = 'abcçdefgğhıijklmnoöprsştuüvyz';
+  const formattedLetters = inputLetters.toLowerCase().trim();
+  let sqlConditions = [];
+
+  for (let i = 0; i < alphabet.length; i++) {
+    const currentLetter = alphabet[i];
+    if (!formattedLetters.includes(currentLetter)) {
+      sqlConditions.push(`kelime NOT LIKE '%${currentLetter}%'`);
+    }
+  }
+  const sql = `SELECT * FROM kelimeler WHERE ${sqlConditions.join(' AND ')}`;
+  return sql;
+}
+
+function canFormWord(word, availableLetters) {
+  word = word.toLowerCase().trim();
+  availableLetters = availableLetters.toLowerCase().trim();
+
+  let checkStatus = false;
+
+  for (let i = 0; i < word.length; i++) {
+    checkStatus = false;
+    const letterOfTheWord = word[i];
+
+    for (let j = 0; j < availableLetters.length; j++) {
+      const availableLetter = availableLetters[j];
+
+      if (letterOfTheWord === availableLetter) {
+        checkStatus = true;
+        availableLetters = availableLetters.replace(availableLetter, '-');
+        break;
+      }
+    }
+    if (!checkStatus) {
+      break;
+    }
+  }
+  return checkStatus;
 }
 
 export const databaseConnection = new DatabaseConnection();
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -9,8 +9,10 @@ import {
   StyleSheet,
   ActivityIndicator,
   Alert,
+  Platform,
 } from 'react-native';
-import {databaseConnection, Word} from '../db/database';
+import {Picker} from '@react-native-picker/picker';
+import {databaseConnection, Word, SearchType} from '../db/database';
 
 interface Section {
   title: string;
@@ -19,9 +21,11 @@ interface Section {
 
 export const HomeScreen: React.FC = () => {
   const [searchText, setSearchText] = useState<string>('');
+  const [searchType, setSearchType] = useState<SearchType>(SearchType.CONTAIN);
   const [sections, setSections] = useState<Section[]>([]);
   const [loading, setLoading] = useState<boolean>(false);
   const [dbInitialized, setDbInitialized] = useState<boolean>(false);
+  const [isError, setIsError] = useState<string>('');
 
   useEffect(() => {
     const initDB = async () => {
@@ -39,10 +43,15 @@ export const HomeScreen: React.FC = () => {
     initDB();
   }, []);
 
+  const renderWord = ({item}: {item: Word}) => (
+    <View style={styles.wordItem}>
+      <Text style={styles.wordText}>{item?.kelime}</Text>
+    </View>
+  );
+
   const groupWordsBySize = (words: Word[]): Section[] => {
     const groupedWords: {[key: number]: Word[]} = {};
 
-    // Kelimeleri boyutlarına göre grupla
     words.forEach(word => {
       if (!groupedWords[word.boyut]) {
         groupedWords[word.boyut] = [];
@@ -50,21 +59,23 @@ export const HomeScreen: React.FC = () => {
       groupedWords[word.boyut].push(word);
     });
 
-    // Section'ları oluştur ve boyuta göre sırala
     return Object.entries(groupedWords)
-      .map(([size, words]) => ({
+      .map(([size, words2]) => ({
         title: `${size} harfli kelimeler`,
-        data: words,
+        data: words2,
       }))
       .sort(
         (a, b) =>
-          parseInt(b.data[0].boyut.toString()) -
-          parseInt(a.data[0].boyut.toString()),
+          parseInt(b.data[0].boyut.toString(), 10) -
+          parseInt(a.data[0].boyut.toString(), 10),
       );
   };
 
   const handleSearch = async () => {
-    if (searchText.trim() === '') return;
+    if (searchText.trim() === '') {
+      setIsError('Henüz harf girmediniz');
+      return;
+    }
     if (!dbInitialized) {
       Alert.alert('Hata', 'Veritabanı henüz hazır değil');
       return;
@@ -72,7 +83,10 @@ export const HomeScreen: React.FC = () => {
 
     setLoading(true);
     try {
-      const results = await databaseConnection.searchWords(searchText);
+      const results = await databaseConnection.searchWords(
+        searchText,
+        searchType,
+      );
       setSections(groupWordsBySize(results));
     } catch (error) {
       Alert.alert('Hata', 'Arama sırasında bir hata oluştu: ' + error);
@@ -81,41 +95,88 @@ export const HomeScreen: React.FC = () => {
     }
   };
 
+  const handleClear = () => {
+    setSearchText('');
+    setSections([]);
+    setIsError('');
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.searchContainer}>
         <TextInput
           style={styles.input}
           value={searchText}
-          onChangeText={setSearchText}
+          onChangeText={text => {
+            setSearchText(text);
+            setIsError('');
+          }}
+          autoCapitalize="none"
+          onFocus={() => setIsError('')}
           placeholder="Harfleri girin..."
           placeholderTextColor="#666"
         />
+      </View>
+      <View style={styles.actionContainer}>
+        <View style={styles.pickerContainer}>
+          <Picker
+            selectedValue={searchType}
+            onValueChange={itemValue => setSearchType(itemValue as SearchType)}
+            style={styles.picker}>
+            <Picker.Item
+              label="Sadece bu harflerden oluşan kelimeleri"
+              value={SearchType.EXACT}
+            />
+            <Picker.Item
+              label="Bu harflerle başlayan kelimeleri"
+              value={SearchType.START}
+            />
+            <Picker.Item
+              label="Bu harflerle biten kelimeleri"
+              value={SearchType.END}
+            />
+            <Picker.Item
+              label="Bu harf öbeğini içeren kelimeleri"
+              value={SearchType.CONTAIN}
+            />
+          </Picker>
+        </View>
         <TouchableOpacity
           style={[styles.button, loading && styles.buttonDisabled]}
           onPress={handleSearch}
           disabled={loading || !dbInitialized}>
-          <Text style={styles.buttonText}>Ara</Text>
+          <Text style={styles.buttonText}>Bul</Text>
         </TouchableOpacity>
       </View>
-
       {loading ? (
-        <ActivityIndicator size="large" color="#0000ff" />
+        <ActivityIndicator size="large" color="#0000ff" style={styles.loader} />
       ) : (
-        <SectionList
-          sections={sections}
-          keyExtractor={item => item.id.toString()}
-          renderItem={({item}) => (
-            <View style={styles.wordItem}>
-              <Text style={styles.wordText}>{item.kelime}</Text>
+        <>
+          {isError ? (
+            <View style={styles.errorContainer}>
+              <Text style={styles.errorText}>{isError}</Text>
             </View>
+          ) : (
+            <SectionList
+              sections={sections}
+              keyExtractor={item => item?.id?.toString()}
+              renderItem={renderWord}
+              renderSectionHeader={({section: {title}}) => (
+                <View style={styles.sectionHeader}>
+                  <Text style={styles.sectionHeaderText}>{title}</Text>
+                </View>
+              )}
+              style={styles.list}
+            />
           )}
-          renderSectionHeader={({section: {title}}) => (
-            <View style={styles.sectionHeader}>
-              <Text style={styles.sectionHeaderText}>{title}</Text>
-            </View>
+          {sections.length > 0 && (
+            <TouchableOpacity
+              style={[styles.clearButton]}
+              onPress={handleClear}>
+              <Text style={styles.buttonText}>Listeyi Temizle</Text>
+            </TouchableOpacity>
           )}
-        />
+        </>
       )}
     </View>
   );
@@ -128,27 +189,39 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
   },
   searchContainer: {
-    flexDirection: 'row',
-    marginBottom: 16,
+    marginBottom: 12,
   },
   input: {
-    flex: 1,
     height: 48,
     borderWidth: 1,
     borderColor: '#ccc',
     borderRadius: 8,
     paddingHorizontal: 16,
-    marginRight: 8,
     fontSize: 16,
+  },
+  picker: {
+    height: Platform.OS === 'ios' ? 150 : 48,
+  },
+  actionContainer: {
+    flexDirection: 'row',
+    marginBottom: 12,
+    gap: 8,
+  },
+  pickerContainer: {
+    flex: 1,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 8,
+    backgroundColor: '#f8f8f8',
   },
   button: {
     backgroundColor: '#007AFF',
-    height: 48,
-    paddingHorizontal: 24,
+    width: 80, // Sabit genişlik
     borderRadius: 8,
     justifyContent: 'center',
     alignItems: 'center',
   },
+
   buttonDisabled: {
     backgroundColor: '#ccc',
   },
@@ -156,6 +229,12 @@ const styles = StyleSheet.create({
     color: '#fff',
     fontSize: 16,
     fontWeight: '600',
+  },
+  loader: {
+    marginTop: 20,
+  },
+  list: {
+    flex: 1,
   },
   wordItem: {
     padding: 16,
@@ -176,5 +255,26 @@ const styles = StyleSheet.create({
     fontSize: 14,
     fontWeight: 'bold',
     color: '#666',
+  },
+  clearButton: {
+    backgroundColor: '#dc3545',
+    height: 48,
+    borderRadius: 8,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 16,
+    marginBottom: Platform.OS === 'ios' ? 20 : 16, // iOS'ta bottom padding ekledik
+  },
+  errorContainer: {
+    flex: 1,
+    // justifyContent: 'center',
+    alignItems: 'center',
+    paddingTop: 20,
+  },
+  errorText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#dc3545',
+    textAlign: 'center',
   },
 });
